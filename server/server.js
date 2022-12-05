f2bd83479259ce2a9d6920ca289ad09507867e00
Odin <leman1911@yandex.ru>
2022-12-05 19:41:28 +0300

    v.1.0.1| add: signUp, signIn, chat;

---
+++ server/server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const app = express();
+const PORT = 4000;
+
+app.use(express.static('./dist'));
+
+app.listen(PORT);
--- src/index.js
+++
@@ -1,4 +0,0 @@
-import {sum} from './modules/sum';
-
-const root = document.querySelector('#root');
-root.textContent = sum(6, -1).toString();
