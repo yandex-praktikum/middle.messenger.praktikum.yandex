60b6ea59c79dbdf7795ff7680146fe7803edf353
Magomed Aliev <magomed157893@yandex.ru>
2022-12-11 15:52:49 +0300

    modified commit

--- express.js
+++ express.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "dist")));
 
