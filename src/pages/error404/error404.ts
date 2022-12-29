3e1c691f7bc4d9dff1f26635fe6bc704764a5d92
Maria <49918972+EstrellaPolaris@users.noreply.github.com>
2022-12-29 10:12:28 +0100

    Update error404.ts

--- src/pages/error404/error404.ts
+++ src/pages/error404/error404.ts
@@ -1,5 +1,5 @@
 import { Block } from '../../utils/Block';
-import Error from '../../components/Error';
+import Error from '../../components/error';
 import template from './error404.hbs';
 import './error404.less';
 
