b70b4bb65adfd0fe60f4cf1e3cca9eb705a3f5c3
Midkhat <midkhat@birdi.com.au>
2023-06-14 01:35:03 +1000

    deploy: readme fix

---
+++ dist/data/data.js
@@ -0,0 +1,306 @@
+export default {
+    chats: [
+        {
+          display_name: "Emma",
+          avatar: "/images/cactus.png",
+          messages: [
+            {
+              author: "You",
+              message: "In the arid desert, a brave cactus stood tall, providing shelter to a weary traveler.",
+              date: "2023-01-03T19:12:57.678Z"
+            },
+            {
+              author: "Emma",
+              message: "During a thunderstorm, a caring cactus collected raindrops to quench its own thirst.",
+              date: "2023-01-07T11:29:56.683Z"
+            },
+            {
+              author: "You",
+              message: "A mischievous rabbit discovered the prickly nature of a cactus the hard way.",
+              date: "2023-02-05T10:56:34.362Z"
+            },
+            {
+              author: "You",
+              message: "A lonely cactus found companionship when a bird built its nest on its spiky branches.",
+              date: "2023-03-24T04:38:45.230Z"
+            },
+            {
+              author: "Emma",
+              message: "In the arid desert, a brave cactus stood tall, providing shelter to a weary traveler.",
+              date: "2023-04-26T12:17:49.036Z"
+            }
+          ],
+          new: 1
+        },
+        {
+          display_name: "Noah",
+          avatar: "/images/cactus.png",
+          messages: [
+            {
+              author: "You",
+              message: "In the arid desert, a brave cactus stood tall, providing shelter to a weary traveler.",
+              date: "2023-02-15T19:40:58.712Z"
+            },
+            {
+              author: "Noah",
+              message: "A lonely cactus found companionship when a bird built its nest on its spiky branches.",
+              date: "2023-03-13T10:38:52.062Z"
+            },
+            {
+              author: "You",
+              message: "A mischievous rabbit discovered the prickly nature of a cactus the hard way.",
+              date: "2023-03-26T04:19:24.473Z"
+            },
+            {
+              author: "You",
+              message: "During a thunderstorm, a caring cactus collected raindrops to quench its own thirst.",
+              date: "2023-04-15T07:55:06.893Z"
+            },
+            {
+              author: "Noah",
+              message: "Amidst the barren landscape, a resilient cactus taught the value of perseverance.",
+              date: "2023-04-30T06:31:09.922Z"
+            },
+            {
+              author: "You",
+              message: "In a land of drought, a wise old cactus shared its water-saving tips with neighboring plants.",
+              date: "2023-05-29T01:35:02.220Z"
+            },
+            {
+              author: "Noah",
+              message: "During a thunderstorm, a caring cactus collected raindrops to quench its own thirst.",
+              date: "2023-05-30T23:06:56.004Z"
+            }
+          ],
+          new: 3
+        },
+        {
+          display_name: "Olivia",
+          avatar: "/images/cactus.png",
+          messages: [
+            {
+              author: "You",
+              message: "During a thunderstorm, a caring cactus collected raindrops to quench its own thirst.",
+              date: "2023-03-25T00:19:35.828Z"
+            }
+          ],
+          new: 0
+        },
+        {
+          display_name: "Liam",
+          avatar: "/images/cactus.png",
+          messages: [
+            {
+              author: "Liam",
+              message: "During a thunderstorm, a caring cactus collected raindrops to quench its own thirst.",
+              date: "2023-01-13T08:27:05.973Z"
+            },
+            {
+              author: "Liam",
+              message: "In the depths of the desert, a hidden oasis bloomed, guarded by a majestic cactus.",
+              date: "2023-02-15T22:10:02.337Z"
+            },
+            {
+              author: "You",
+              message: "Amidst the barren landscape, a resilient cactus taught the value of perseverance.",
+              date: "2023-03-30T18:10:46.175Z"
+            },
+            {
+              author: "You",
+              message: "Once upon a time, there was a tiny cactus who dreamt of exploring the desert.",
+              date: "2023-06-06T05:05:37.421Z"
+            }
+          ],
+          new: 2
+        },
+        {
+          display_name: "Ava",
+          avatar: "/images/cactus.png",
+          messages: [
+            {
+              author: "Ava",
+              message: "During a thunderstorm, a caring cactus collected raindrops to quench its own thirst.",
+              date: "2023-03-06T01:10:37.354Z"
+            },
+            {
+              author: "You",
+              message: "During a thunderstorm, a caring cactus collected raindrops to quench its own thirst.",
+              date: "2023-05-26T13:45:38.646Z"
+            }
+          ],
+          new: 1
+        },
+        {
+          display_name: "Isabella",
+          avatar: "/images/cactus.png",
+          messages: [
+            {
+              author: "Isabella",
+              message: "Under the scorching sun, a blooming cactus proudly displayed its vibrant flowers.",
+              date: "2023-03-08T17:33:29.677Z"
+            },
+            {
+              author: "You",
+              message: "In the arid desert, a brave cactus stood tall, providing shelter to a weary traveler.",
+              date: "2023-04-20T02:58:48.322Z"
+            },
+            {
+              author: "Isabella",
+              message: "In a land of drought, a wise old cactus shared its water-saving tips with neighboring plants.",
+              date: "2023-04-23T09:24:13.754Z"
+            },
+            {
+              author: "You",
+              message: "Once upon a time, there was a tiny cactus who dreamt of exploring the desert.",
+              date: "2023-05-25T07:18:38.405Z"
+            }
+          ],
+          new: 1
+        },
+        {
+          display_name: "Sophia",
+          avatar: "/images/cactus.png",
+          messages: [
+            {
+              author: "You",
+              message: "In the arid desert, a brave cactus stood tall, providing shelter to a weary traveler.",
+              date: "2023-02-02T02:39:12.574Z"
+            },
+            {
+              author: "Sophia",
+              message: "A lonely cactus found companionship when a bird built its nest on its spiky branches.",
+              date: "2023-03-11T18:34:14.018Z"
+            },
+            {
+              author: "Sophia",
+              message: "A curious child marveled at the unique shapes and sizes of cacti in a botanical garden.",
+              date: "2023-04-22T14:50:42.703Z"
+            },
+            {
+              author: "You",
+              message: "During a thunderstorm, a caring cactus collected raindrops to quench its own thirst.",
+              date: "2023-05-19T04:00:33.522Z"
+            },
+            {
+              author: "You",
+              message: "A curious child marveled at the unique shapes and sizes of cacti in a botanical garden.",
+              date: "2023-05-28T11:48:46.729Z"
+            }
+          ],
+          new: 4
+        },
+        {
+          display_name: "Mia",
+          avatar: "/images/cactus.png",
+          messages: [
+            {
+              author: "You",
+              message: "Amidst the barren landscape, a resilient cactus taught the value of perseverance.",
+              date: "2023-03-30T05:21:14.558Z"
+            },
+            {
+              author: "Mia",
+              message: "A curious child marveled at the unique shapes and sizes of cacti in a botanical garden.",
+              date: "2023-04-17T23:29:15.530Z"
+            },
+            {
+              author: "You",
+              message: "Amidst the barren landscape, a resilient cactus taught the value of perseverance.",
+              date: "2023-05-05T13:08:11.878Z"
+            },
+            {
+              author: "Mia",
+              message: "A curious child marveled at the unique shapes and sizes of cacti in a botanical garden.",
+              date: "2023-05-09T05:39:33.851Z"
+            }
+          ],
+          new: 1
+        },
+        {
+          display_name: "Charlotte",
+          avatar: "/images/cactus.png",
+          messages: [
+            {
+              author: "Charlotte",
+              message: "Once upon a time, there was a tiny cactus who dreamt of exploring the desert.",
+              date: "2023-01-18T20:17:28.299Z"
+            },
+            {
+              author: "Charlotte",
+              message: "A lonely cactus found companionship when a bird built its nest on its spiky branches.",
+              date: "2023-02-03T10:36:22.749Z"
+            },
+            {
+              author: "You",
+              message: "Amidst the barren landscape, a resilient cactus taught the value of perseverance.",
+              date: "2023-04-14T07:43:17.451Z"
+            },
+            {
+              author: "Charlotte",
+              message: "During a thunderstorm, a caring cactus collected raindrops to quench its own thirst.",
+              date: "2023-04-23T09:07:42.744Z"
+            },
+            {
+              author: "You",
+              message: "In a land of drought, a wise old cactus shared its water-saving tips with neighboring plants.",
+              date: "2023-04-28T10:24:11.567Z"
+            },
+            {
+              author: "You",
+              message: "During a thunderstorm, a caring cactus collected raindrops to quench its own thirst.",
+              date: "2023-05-03T15:33:42.855Z"
+            }
+          ],
+          new: 5
+        },
+        {
+          display_name: "Amelia",
+          avatar: "/images/cactus.png",
+          messages: [
+            {
+              author: "Amelia",
+              message: "Amidst the barren landscape, a resilient cactus taught the value of perseverance.",
+              date: "2023-01-07T03:29:15.344Z"
+            },
+            {
+              author: "You",
+              message: "Once upon a time, there was a tiny cactus who dreamt of exploring the desert.",
+              date: "2023-01-08T13:34:43.110Z"
+            },
+            {
+              author: "You",
+              message: "A mischievous rabbit discovered the prickly nature of a cactus the hard way.",
+              date: "2023-03-19T19:15:22.392Z"
+            },
+            {
+              author: "You",
+              message: "Under the scorching sun, a blooming cactus proudly displayed its vibrant flowers.",
+              date: "2023-04-08T15:22:10.570Z"
+            },
+            {
+              author: "Amelia",
+              message: "A lonely cactus found companionship when a bird built its nest on its spiky branches.",
+              date: "2023-05-14T15:44:20.893Z"
+            },
+            {
+              author: "Amelia",
+              message: "A mischievous rabbit discovered the prickly nature of a cactus the hard way.",
+              date: "2023-06-06T16:26:46.965Z"
+            }
+          ],
+          new: 0
+        }
+      ],
+    profile : {
+        first_name: "Random",
+        second_name: "Hero",
+        avatar: "/images/hero.png",
+        login: "RandomHero",
+        email: "random@hero.com",
+        epassword: "admin",
+        phone: "+1 123-456-7890",
+        age: 99,
+        city: "New York"
+    }
+    
+}
