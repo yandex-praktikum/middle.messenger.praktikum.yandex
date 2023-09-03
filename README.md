756dd61ce068e40b56b62dc32f2252f7f6102e24
Evgeny Talagaev <talagaev@r52.ru>
2023-09-04 00:21:55 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
+«[Ссылка на прототип](https://www.figma.com/file/zoLY1xVwlhbMUxpHslGQDp/middle.messenger.praktikum.yandex?type=design&t=ruOBLon7eRgVs26J-6)»
 
+## Установка
+
+- `npm install` — инициализация проекта,
+- `npm run start` — сборка проекта и запуск сервера (http://localhost:3000),
+- `npm run dev` — запуск версии для разработки,
+- `npm run build` — сборка проекта.
+
+## Структура
+
+- /src/layout/ - макет
+- /src/layout/mixins.pug - собираем все компоненты
+- /src/pages/ - шаблоны страниц
+- /src/components/- компоненты
+- /src/scss/ - стили проекта
+- /src/scss/style.scss - собираем все стили
+
+## Ключевые технологии
+
+- шаблонизатор - pug
+- css препроцессор - sass
---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import { resolve } from 'path';
+import { defineConfig } from "vite"
+
+import vitePugPlugin from 'vite-plugin-pug-transformer'
+
+const locals = { bundler: 'Vite' };
+const root = resolve(__dirname, 'src');
+
+export default defineConfig({
+  root,
+  base: '',
+  plugins: [vitePugPlugin({ pugLocals: locals })],
+  build: {
+    outDir: resolve(__dirname, 'build'),
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        singIn: resolve(root, 'index.html'),
+        registration: resolve(root, 'registration.html'),
+        chatsAndChat: resolve(root, 'chats-and-chat.html'),
+        userSettings: resolve(root, 'user-settings.html'),
+        404: resolve(root, '404.html'),
+        500: resolve(root, '500.html'),
+      },
+    }
+  },
+  publicDir: resolve(__dirname, 'static'),
+})
