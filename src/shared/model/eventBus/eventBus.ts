ade87a569599344b9e10d1eacaa036d4b269e8b1
Ratmir Tsaregorodtsev <RTsaregorodtsev@alabuga.ru>
2023-09-12 15:31:30 +0300

    Add eventBus

---
+++ src/shared/model/eventBus/eventBus.ts
@@ -0,0 +1,46 @@
+type Subscribers = {
+  [key: string]: Function[];
+};
+
+type Unregister = () => void;
+
+interface IEventBus {
+  dispatch(event: string, arg: any): void;
+  register(event: string, callback: Function): Unregister;
+  unregister(event: string, callback: Function): void;
+}
+
+export class EventBus implements IEventBus {
+  private subscribers: Subscribers;
+
+  constructor() {
+    this.subscribers = {};
+  }
+
+  dispatch(event: string, ...args: any): void {
+    if (this.subscribers[event] === undefined) {
+      return;
+    }
+    this.subscribers[event].forEach((subscriber) => {
+      subscriber(...args);
+    });
+  }
+
+  register(event: string, callback: Function): Unregister {
+    if (this.subscribers[event] === undefined) {
+      this.subscribers[event] = [];
+    }
+
+    this.subscribers[event].push(callback);
+  }
+
+  unregister(event: string, callback: Function): void {
+    if (this.subscribers[event] === undefined) {
+      return;
+    }
+
+    this.subscribers[event] = this.subscribers[event].filter(
+      (subscriber) => subscriber !== callback
+    );
+  }
+}
