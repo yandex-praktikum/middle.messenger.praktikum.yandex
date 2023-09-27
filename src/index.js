517a03dd96245d49a7b6cdad0d781e0d39e88b07
ZaoBaoo <yurkin.daniil96@gmail.com>
2023-09-27 23:21:32 +0600

    Первая реализация SPA

--- src/index.js
+++ src/index.js
@@ -9,31 +9,51 @@ import {ErrorPage} from "./pages/ErrorPage/index.js";
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.querySelector('#app');
-    const route = window.location.pathname;
+
 
     console.log("JS RUN");
-    console.log(route);
 
     if(!root) return;
 
-    switch (route) {
-        case "/":
-            root.innerHTML = Main({content: 'main'});
-            break;
-        case "/login":
-            root.innerHTML = Login({content: 'login'});
-            break;
-        case "/signin":
-            root.innerHTML = Signin({content: 'signin'});
-            break;
-        case "/profile":
-            root.innerHTML = Profile({content: 'profile'});
-            break;
-        default:
-            root.innerHTML = ErrorPage({content: '404'});
-            break
+    const definitionRoute = () => {
+        const route = window.location.pathname;
+
+        switch (route) {
+            case "/":
+                root.innerHTML = Main({content: 'main'});
+                break;
+            case "/login":
+                root.innerHTML = Login({content: 'login'});
+                break;
+            case "/signin":
+                root.innerHTML = Signin({content: 'signin'});
+                break;
+            case "/profile":
+                root.innerHTML = Profile({content: 'profile'});
+                break;
+            default:
+                root.innerHTML = ErrorPage({content: '404'});
+                break
+        }
+
+        handlerRoute()
     }
 
+    const handlerRoute = () => {
+        const allLink = document.querySelectorAll('a');
+
+        allLink.forEach(link => {
+            link.addEventListener('click', (e) => {
+                e.preventDefault()
+                const path = e.currentTarget.getAttribute('data-page');
+                history.pushState({ page: path }, "", path);
+
+                definitionRoute()
+            })
+        })
+    }
+
+    definitionRoute()
 })
 
 
