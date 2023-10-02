bb8cb6b41d9916b76fbf9cc072aa42c974409a84
Mila Vizelka <l.viselka@vk.teams>
2023-10-02 23:47:11 +0300

    fixed test

--- src/pages/sign-in/tmpl/content.tmpl.ts
+++ src/pages/sign-in/tmpl/content.tmpl.ts
@@ -1,18 +1,19 @@
  export const content = `
-   <div class="wrapper-sign-in-page">
-      <header class="header">
-         {{{ logo }}}
-      </header>
-      
-      <div class="wrapper-content">
-         <form class="form">
-           {{{ title }}}
-           {{{ input }}}
-           {{{ button }}}
-         </form>
-      
-         {{{ signUpPageLink }}}
-      </div>
-   </div>
+    <div class="wrapper-sign-in-page">
+       <header class="header">
+          {{{ logo }}}
+       </header>
+       
+       <div class="wrapper-content">
+          <form class="form">
+            {{{ title }}}
+            {{{ input }}}
+            {{{ button }}}
+          </form>
+       
+          {{{ signUpPageLink }}}
+       </div>
+    </div>
  `
+ 
  
--- src/pages/sign-up/tmpl/content.tmpl.ts
+++ src/pages/sign-up/tmpl/content.tmpl.ts
@@ -15,3 +15,4 @@ export const content = `
       </div>
   </div>
 `
+
