eb8ec2952170fd83cde0eb0b697bdac6d6916ce0
Evgeny Talagaev <talagaev@r52.ru>
2023-10-16 18:15:26 +0300

    Fix express error

--- server.ts
+++ server.ts
@@ -1,9 +1,22 @@
-import express from 'express'; // eslint-disable-line
+import path from 'path';
+import { fileURLToPath } from 'url';
+import express from 'express';
+import cors from 'cors';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = 3000;
 
+app.use(cors());
+
 app.use(express.static('build'));
+
+// @ts-ignore
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build/index.html'))
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
