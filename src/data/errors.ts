379b9b457e8ee0320162384f2445a190ce18fb7c
Denis Zheleznov <zhel1@yandex.ru>
2023-12-23 17:40:28 -0800

    bug fix on page 500

--- src/data/errors.ts
+++ src/data/errors.ts
@@ -6,6 +6,6 @@ export const Error404 = {
 
 export const Error500 = {
     error: 500,
-    errorText: "Oops! This Page Could Not Be Found",
-    message: "sorry but the page you are looking for does not exist, have been removed. name changed or is temporarily unavailable",
+    errorText: "Oops! Internal Server Error",
+    message: "The server encountered an internal error or misconfiguration and was unable to complete your request.",
 }
