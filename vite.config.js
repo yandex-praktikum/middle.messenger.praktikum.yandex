cea1a96431f83bafab3506422d41177865ada114
Dmitriy o_HET <dimati9@mail.ru>
2024-02-01 23:04:35 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,15 @@ import {defineConfig} from 'vite'
 import handlebars from 'vite-plugin-handlebars';
 
 export default defineConfig({
+    publicDir: "static",
     plugins: [handlebars()],
+    css: {
+        preprocessorOptions: {
+            less: {
+                math: "always",
+                relativeUrls: true,
+                javascriptEnabled: true
+            },
+        },
+    }
 })
