ea34d364a18cd560169f5631f8354e02e5dabb94
Alucard <emran01@yandex.ru>
2024-02-06 10:08:36 +0300

    Sprint_1: Настроил vite, установил handlebars, scss

---
+++ Messager/vite.config.ts
@@ -0,0 +1,21 @@
+import {resolve} from 'path'
+import { defineConfig } from 'vite'
+import handlebars from 'vite-plugin-handlebars'
+
+
+
+
+export default defineConfig({
+    root: resolve(__dirname,'src'),
+    build:{
+        outDir:resolve(__dirname, 'dist')
+    },
+
+//@ts-ignore
+    plugins:[handlebars({
+        partialDirectory:[
+            resolve(__dirname, 'src/components'),
+
+        ]
+    })]
+})
