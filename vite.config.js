9d4e2945bf77ede817b2476507ee879414226265
Dmitriy o_HET <dimati9@mail.ru>
2024-02-07 01:17:04 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,21 @@ import handlebars from 'vite-plugin-handlebars';
 
 export default defineConfig({
     root: "./src",
+    build: {
+        rollupOptions: {
+            input: './src/index.html',
+            output:
+                {
+                    format: 'es',
+                    strict: false,
+                    dir: 'dist/',
+                    entryFileNames: `assets/[name].js`,
+                    chunkFileNames: `assets/[name].js`,
+                    assetFileNames: `assets/[name].[ext]`,
+                }
+        }
+    },
     plugins: [
         handlebars(),
     ],
-    css: {
-        preprocessorOptions: {
-            less: {
-                math: "always",
-                relativeUrls: true,
-                javascriptEnabled: true
-            },
-        },
-    }
 })
