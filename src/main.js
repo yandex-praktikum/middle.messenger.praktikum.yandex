8f002d86810c362fb3a1357862b60824d448bf6d
Dmitriy o_HET <dimati9@mail.ru>
2024-02-09 21:20:25 +0500

    edit routes

--- src/main.js
+++ src/main.js
@@ -19,6 +19,10 @@ document.addEventListener("DOMContentLoaded", () => {
             return showMessages();
         }  else if (path === "/profile") {
             return showProfile();
+        }   else if (path === "/profile-edit") {
+            return showProfile("edit");
+        }   else if (path === "/profile-edit-password") {
+            return showProfile("password");
         }   else if (path === "/500") {
             return showError(500);
         }   else {
