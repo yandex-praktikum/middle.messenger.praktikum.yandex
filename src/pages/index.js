3745ead469c76cc43b2d7a58588ce81201fea381
anna <ann_muyer111@mail.ru>
2024-03-14 14:55:44 +0300

    sprint_1 | Проект

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import './style.scss';
+export { LoginPage } from './login-page';
+export { RegistrationPage } from './registration-page';
+export { ProfilePage } from './profile-page';
+export { EditProfilePage } from './edit-profile-page';
+export { EditPasswordPage } from './edit-password-page';
+export { NotFoundPage } from './not-found-page';
+export { ErrorPage } from './error-page';
---
+++ src/pages/registration-page/index.js
@@ -0,0 +1,1 @@
+export { default as RegistrationPage } from './registration-page.hbs?raw';
