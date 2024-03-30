b3bdae463dda5a682446ac72e35d1bf11a2cd6b9
dedkovnd <dedkovnd@gmail.com>
2024-03-30 23:17:54 +0300

    test43

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -3,4 +3,4 @@ export { LoginPage } from './login-page';
 export { RegistrationPage } from './registration-page';
 export { ProfilePage } from './profile-page';
 export { ErrorPage } from './error404';
-export { Error500 } from './error500'
+export { Error500 } from './error500';
