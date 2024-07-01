683780a1b22a21b13c62d2b3534cec450483595b
Daniil <77277774+shamemask@users.noreply.github.com>
2024-07-01 17:39:18 +0300

    handlebars и настройки для netlify

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { createHtmlPlugin } from 'vite-plugin-html';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default {
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      minify: true,
+      inject: {
+        data: {
+          title: 'My Messenger'
+        },
+      },
+    }),
+  ],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "src/styles/global.scss";`
+      }
+    }
+  }
+};
