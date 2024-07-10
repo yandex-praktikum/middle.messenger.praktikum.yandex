24769ce506ef825c4e21c55863c8a3540f3dc7d9
Wycctrujjla109121 <56939296+Wycctrujjla109121@users.noreply.github.com>
2024-07-10 04:36:41 +0300

    feat: переместил папки с index.html

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,21 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite'
 import handlebars from 'vite-plugin-handlebars';
 
+const root = resolve(__dirname, 'src/pages')
+const outDir = resolve(__dirname, 'dist')
+
 export default defineConfig({
     plugins: [handlebars()],
-}) 
+    root,
+    build: {
+        outDir,
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                main: resolve(root, 'index.html'),
+                registration: resolve(root, 'registration.html'),
+            }
+        }
+    }
+})
