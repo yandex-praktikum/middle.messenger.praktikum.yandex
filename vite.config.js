a5dbb008444fd8396cc915bcf339a3d0d61e595d
Daniil Pereskokov <129772326+Daniil8565@users.noreply.github.com>
2024-07-14 09:21:05 +0300

    Изменение в vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,24 @@ export default defineConfig({
   root: resolve(__dirname, 'src'),
   build: {
     outDir: resolve(__dirname, 'dist'),
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, './src/index.html'),
+        entrance: resolve(__dirname, './src/entrance/entrance.html'),
+        registration: resolve(
+          __dirname,
+          './src/registration/registration.html'
+        ),
+        error: resolve(__dirname, './src/404/404.html'),
+        error2: resolve(__dirname, './src/500/500.html'),
+        profile: resolve(__dirname, './src/profile/profile.html'),
+        changeData: resolve(__dirname, './src/changeData/changeData.html'),
+        changePassword: resolve(
+          __dirname,
+          './src/changePassword/changePassword.html'
+        ),
+      },
+    },
   },
   server: {
     port: 3000,
