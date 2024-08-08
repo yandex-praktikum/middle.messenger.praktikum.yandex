25324e98ed8591da13c78bcd737a9a3457ab6c0f
yaqazard <yaqazard@yandex.ru>
2024-08-08 18:58:41 +0400

    fixik 1758

--- src/components/list-chatmy/index.ts
+++ src/components/list-chatmy/index.ts
@@ -6,7 +6,7 @@ export {default as ListChatmy} from './list-chatmy.hbs?raw';
 
 
 
-Handlebars.registerHelper('cardList', (string)=>{
+Handlebars.registerHelper('cardList', ()=>{
     return [{nameuser: 'ЕГОР', messageTime:'Вчера 16:00',messageText: 'Привет Как дела?'}, 
             {nameuser: 'Вадим', messageTime:'Вчера, 17:00',messageText: 'Все лучше чем у всех. Спринт пишу.'},
             {nameuser: 'Егор', messageTime:'Вчера, 17:15',messageText: 'Молодец'}, 
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import Handlebars from 'handlebars';
 import * as Components from './components';
 import * as Pages from './pages';
 
-console.log('temp: ', Pages.ListPages);
-
 const pages = {
   'login': [Pages.LoginPage, { test: '12345' }],
   'nav': [Pages.NavigatePage],
@@ -26,8 +24,6 @@ function navigate(page: string) {
 
   const templatingFunction = Handlebars.compile(source);
 
-  console.log('html', templatingFunction(context));
-
   container.innerHTML = templatingFunction(context);
 }
 
@@ -43,28 +39,3 @@ document.addEventListener('click', e => {
     e.stopImmediatePropagation();
   }
 });
-
-// import './style.css'
-// import typescriptLogo from './typescript.svg'
-// import viteLogo from '/vite.svg'
-// import { setupCounter } from './counter.ts'
-
-// document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://www.typescriptlang.org/" target="_blank">
-//       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-//     </a>
-//     <h1>Vite + TypeScript</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite and TypeScript logos to learn more
-//     </p>
-//   </div>
-// `
-
-// setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
